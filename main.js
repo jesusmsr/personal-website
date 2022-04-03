4a7b97e31da6a84ff8df62637bcc55da233c3db4
jesusmsr <jsr20xx@gmail.com>
2022-04-03 18:10:31 +0200

    Create home page and about me

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 import './style.css'
+import $ from 'jquery';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+$(document).ready(function () {
+  var scroll_pos = 0;
+  window.addEventListener("scroll", function(){
+    
+    console.log(window.scrollY);
+    if (this.window.scrollY > 300){
+      
+    }
+  })
+
+  
+  /*
+  $(document).scroll(function () {
+    
+    scroll_pos = $(this).scrollTop();
+
+    const step = 255 / $('.bottom-section').height();
+    const multiplier = Math.round(
+      $('.bottom-section').height() /
+      $('.bottom-section').parent().height()
+    );
+
+    const currentStyle = $('.bottom-section').css('backgroundColor');
+    const rgbValues = currentStyle.substring(
+      currentStyle.lastIndexOf("(") + 1,
+      currentStyle.lastIndexOf(")")
+    );
+
+    const newValue = step * scroll_pos * multiplier;
+
+    $('.bottom-section').css('background-color',`rgb(${newValue}, ${newValue}, ${newValue})`);
+    
+  })
+  */
+});
