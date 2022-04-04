2ad53366925943a0d76e68b1227a9614c8243305
jesusmsr <jsr20xx@gmail.com>
2022-04-04 20:23:00 +0200

    Add parallax

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ import './style.css'
 import $ from 'jquery';
 
 $(document).ready(function () {
+
   var scroll_pos = 0;
   var fadeRatio = 0.4;
-  window.addEventListener("scroll", function () {
+
+  window.addEventListener("scroll", function (e) {
+    //Fade on scroll
     var value = 233 - (window.scrollY * fadeRatio);
     value <= 10 ? value = 10 : value = value;
     $('.bottom-section').css('background-color', `rgb(${value}, ${value}, ${value})`);
+
+    //Parallax 
+    const parallaxTarget = document.querySelectorAll('.scroll');
+    var scrolled = window.pageYOffset;
+
+    var index = 0, length = parallaxTarget.length;
+    for (index; index < length; index++) {
+      var pos = window.pageYOffset * parallaxTarget[index].dataset.rate;
+      parallaxTarget[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px';
+    }
+
   });
+
 });
