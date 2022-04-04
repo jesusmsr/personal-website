b991595a251744fb3aec5db271278f7b645c346a
jesusmsr <jsr20xx@gmail.com>
2022-04-04 18:53:26 +0200

    Create fading effect in bottom section

--- main.js
+++ main.js
@@ -3,36 +3,10 @@ import $ from 'jquery';
 
 $(document).ready(function () {
   var scroll_pos = 0;
-  window.addEventListener("scroll", function(){
-    
-    console.log(window.scrollY);
-    if (this.window.scrollY > 300){
-      
-    }
-  })
-
-  
-  /*
-  $(document).scroll(function () {
-    
-    scroll_pos = $(this).scrollTop();
-
-    const step = 255 / $('.bottom-section').height();
-    const multiplier = Math.round(
-      $('.bottom-section').height() /
-      $('.bottom-section').parent().height()
-    );
-
-    const currentStyle = $('.bottom-section').css('backgroundColor');
-    const rgbValues = currentStyle.substring(
-      currentStyle.lastIndexOf("(") + 1,
-      currentStyle.lastIndexOf(")")
-    );
-
-    const newValue = step * scroll_pos * multiplier;
-
-    $('.bottom-section').css('background-color',`rgb(${newValue}, ${newValue}, ${newValue})`);
-    
-  })
-  */
+  var fadeRatio = 0.4;
+  window.addEventListener("scroll", function () {
+    var value = 233 - (window.scrollY * fadeRatio);
+    value <= 10 ? value = 10 : value = value;
+    $('.bottom-section').css('background-color', `rgb(${value}, ${value}, ${value})`);
+  });
 });
