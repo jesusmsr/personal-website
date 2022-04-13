c9f8839222206c496fec1b8cbc3f489c0a761c11
jesusmsr <jsr20xx@gmail.com>
2022-04-13 16:35:05 +0200

    Add parallax and make design responsive for pc

--- main.js
+++ main.js
@@ -2,31 +2,22 @@ import './style.css'
 import $ from 'jquery';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'animate.css';
 
 $(document).ready(function () {
 
-  var scroll_pos = 0;
-  var fadeRatio = 0.4;
-
   AOS.init();
 
+  function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+
+
+    item.style.transform = `translateY(${distance * speed}px)`;
+  }
+
   window.addEventListener("scroll", function (e) {
-    //Fade on scroll
-    var value = 233 - (window.scrollY * fadeRatio);
-    value <= 10 ? value = 10 : value = value;
-    $('.bottom-section').css('background-color', `rgb(${value}, ${value}, ${value})`);
-
-    //Parallax 
-    console.log('asd');
-    const parallaxTarget = document.querySelectorAll('.scroll');
-    var scrolled = window.pageYOffset;
-
-    var index = 0, length = parallaxTarget.length;
-    for (index; index < length; index++) {
-      var pos = window.pageYOffset * parallaxTarget[index].dataset.rate;
-      parallaxTarget[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px';
-    }
-
+    parallax('.scroll', window.scrollY, 1);
   });
 
 });
+
