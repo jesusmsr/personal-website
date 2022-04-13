e0b5776936d2c7f4b5209d70b7defbd63db15626
jesusmsr <jsr20xx@gmail.com>
2022-04-13 18:15:15 +0200

    Add Kute library and add blob

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import $ from 'jquery';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import KUTE from 'kute.js'
 
 $(document).ready(function () {
 
@@ -13,13 +14,43 @@ $(document).ready(function () {
   }
 
   if (window.screen.width >= 320) {
-    const item = document.querySelector('.main-container').style.position = 'fixed';
-  } else {
-    window.addEventListener("scroll", function (e) {
-      parallax('.scroll', window.scrollY, 1);
-    });
+    document.querySelector('.main-container').style.position = 'fixed';
+    document.querySelector('.scroll').style.position = 'fixed';
   }
 
+  const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { easing: "easingCubicInOut", duration: 1500 }
+  )
+
+  const tween2 = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob2' },
+    { path: '#blob3' },
+    { easing: "easingCubicInOut", duration: 2000 }
+  )
+
+  const tween3 = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob3' },
+    { path: '#blob2' },
+    { easing: "easingCubicInOut", duration: 3000 }
+  )
+
+  const tween4 = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob2' },
+    { path: '#blob1' },
+    { easing: "easingCubicInOut", duration: 2000 }
+  )
+
+  tween.chain(tween2);
+  tween2.chain(tween3);
+  tween3.chain(tween4);
+  tween4.chain(tween);
+  tween.start();
 
 });
 
